1ae37744c0ee966571efd8ca812563ba34aa0bc0
Edwin Sair Mesa Cetina <edwin.mesa@grupodist.com>
2020-03-13 15:57:08 -0500

    changes Faker labs

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Post, Body, Res,HttpStatus, Get } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UserController {
-    constructor (private userService: UserService){}
+    constructor(private userService: UserService) { }
 
     @Post('/createuser')
-    async createUser(@Res() res, @Body() createUserDto: CreateUserDto){
+    async createUser(@Res() res, @Body() createUserDto: CreateUserDto) {
         const user = await this.userService.addUser(createUserDto);
         return res.status(HttpStatus.OK).json({
             msg: "User Create Successfully",
@@ -16,9 +16,9 @@ export class UserController {
     }
 
     @Get('/allusers')
-    async getUsers(@Res() res){
+    async getUsers(@Res() res) {
         const users = await this.userService.getUsers();
         return res.status(HttpStatus.OK).json(users)
     }
-    
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/user.dto';
+var faker = require('faker');
+faker.locale = "es";
+
 
 @Injectable()
 export class UserService {
@@ -12,8 +15,20 @@ export class UserService {
         private readonly userRepository: Repository<Users>
     ) { }
 
-    async addUser(createUserDto: CreateUserDto): Promise<Users>{
-        return await this.userRepository.save(createUserDto);
+    async addUser(createUserDto: CreateUserDto): Promise<Users> {
+
+        /**Para Testeo */
+        for (let i = 0; i < 10000; i++) {
+            const newUser = new Users();
+            newUser.username = faker.internet.userName();
+            newUser.fullName = faker.name.firstName() + ' ' + faker.name.lastName();
+            newUser.email = faker.internet.email();
+            newUser.age = faker.random.number();
+            newUser.password = faker.internet.password();
+            return await this.userRepository.save(newUser);
+        }
+        /**Original Code */
+        // return await this.userRepository.save(createUserDto);
     }
 
     async getUsers(): Promise<Users[]> {
