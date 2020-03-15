49d65e55b9993f8fa66ca192e923c213dd3b4300
edwinsar <edwinsar.mes@gmail.com>
2020-03-14 23:02:09 -0500

    add funtions users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Res, HttpStatus, Get } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus, Get, Param, Put, Query, NotFoundException, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/user.dto';
 
@@ -6,19 +6,63 @@ import { CreateUserDto } from './dto/user.dto';
 export class UserController {
     constructor(private userService: UserService) { }
 
-    @Post('/createuser')
+    @Post('')
     async createUser(@Res() res, @Body() createUserDto: CreateUserDto) {
-        const user = await this.userService.addUser(createUserDto);
-        return res.status(HttpStatus.OK).json({
-            msg: "User Create Successfully",
-            user
-        })
+        try {
+            const user = await this.userService.addUser(createUserDto);
+            return res.status(HttpStatus.OK).json({
+                msg: "User Create Successfully",
+                user
+            })
+        } catch (error) {
+            if (error) throw new NotFoundException('Error at created user');
+        }
     }
 
-    @Get('/allusers')
+    @Get('')
     async getUsers(@Res() res) {
-        const users = await this.userService.getUsers();
-        return res.status(HttpStatus.OK).json(users)
+        try {
+            const users = await this.userService.getUsers();
+            return res.status(HttpStatus.OK).json(users)
+        } catch (error) {
+            if (error) throw new NotFoundException('Error in Query');
+        }
+    }
+
+    @Get(':id')
+    async getUser(@Res() res, @Param('id') id) {
+        try {
+            const user = await this.userService.getUser(id);
+            return res.status(HttpStatus.OK).json(user);
+        } catch (error) {
+            if (error) throw new NotFoundException('User does not exist');
+        }
+    }
+
+    @Put(':id')
+    async updateUser(@Res() res, @Body() createUserDto: CreateUserDto, @Param('id') id) {
+        try {
+            const updateUser = await this.userService.updateUser(id, createUserDto);
+            return res.status(HttpStatus.OK).json({
+                msg: "User Update Successfully",
+                updateUser
+            });
+        } catch (error) {
+            if (error) throw new NotFoundException('User does not exist');
+        }
+    }
+
+    @Delete(':id')
+    async deletUser(@Res() res, @Param('id') id) {
+        try {
+            const deletUser = await this.userService.deleteUser(id);
+            return res.status(HttpStatus.OK).json({
+                msg: 'User Deleted Successfully',
+                deletUser
+            });
+        } catch (error) {
+            if (error) throw new NotFoundException('User cant not delete');
+        }
     }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -16,8 +16,7 @@ export class UserService {
     ) { }
 
     async addUser(createUserDto: CreateUserDto): Promise<Users> {
-
-        /**Para Testeo */
+        /**For Test */
         for (let i = 0; i < 10000; i++) {
             const newUser = new Users();
             newUser.username = faker.internet.userName();
@@ -34,6 +33,22 @@ export class UserService {
     async getUsers(): Promise<Users[]> {
         const users = await this.userRepository.find();
         return users;
+    }
+
+    async getUser(id: number): Promise<Users> {
+        const user = await this.userRepository.findOne(id);
+        return user;
+    }
+
+    async updateUser(id: number, createUserDto: CreateUserDto): Promise<Users> {
+        const userId = await this.userRepository.findOne(id);
+        const updateUser = Object.assign(userId, createUserDto);
+        return await this.userRepository.save(updateUser);
+    }
+
+    async deleteUser(id: number): Promise<any> {
+        return await this.userRepository.delete(id);
+        // return deleteUser;
     }
 
 }
